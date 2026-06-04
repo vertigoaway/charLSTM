0529e95894f8024005ad5bbe1bc426472aaf7623
vertigoaway <connormm2@protonmail.com>
2026-06-04 18:20:54 -0400

    Run chat only if main file

--- chat.py
+++ chat.py
@@ -1,5 +1,9 @@
 import charLSTM as clstm
 import trinketbox.ai.utils.outProcessing as post
 
-print('starting terminal interface')
-post.basicInterface(clstm.model,clstm.vocab,timeSteps=clstm.inSize)
+#TODO: use command line args for choosing what weights to load
+if __name__=='__main__':
+    model = clstm.create()
+    model.loadWeights(clstm.modelPath)
+    print('starting terminal interface')
+    post.basicInterface(model,clstm.vocab,timeSteps=clstm.inSize)
