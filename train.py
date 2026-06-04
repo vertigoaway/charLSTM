fd083059a812feec99d5a5cb622fc378b54afef0
vertigoaway <connormm2@protonmail.com>
2026-06-04 18:22:24 -0400

    Use new save/load & Housekeeping

--- train.py
+++ train.py
@@ -3,38 +3,50 @@ import charLSTM as clstm
 from torch import nn
 import torch
 
-model = clstm.model
+#TODO: use command line args
+#TODO: write docs fo ts
 
 
-batch_size : int = 20
-epochs : int = 10
-trainingDataPath : str = "data.csv"
+def main():
+    model = clstm.create()
+    try:
+        model.loadWeights(clstm.modelPath)
+    except:
+        print('load failed')
 
-lossFn = nn.CrossEntropyLoss()
-learning_rate : float = 5e-4
+    batch_size : int = 20
+    epochs : int = 10
+    trainingDataPath : str = "data.csv"
 
-optimizer = torch.optim.Adam(clstm.model.parameters(), lr=learning_rate)
+    lossFn = nn.CrossEntropyLoss()
+    learning_rate : float = 5e-4
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 
 
-train_dataloader,test_dataloader = clstm.loadTrainAndTestData(trainingDataPath=trainingDataPath,batch_size=batch_size)
-loopdeloop = loops.trainAndTest(train_dataloader,
-                                test_dataloader,
-                                clstm.model,
-                                lossFn,
-                                optimizer)
+    train_dataloader,test_dataloader = clstm.loadTrainAndTestData(trainingDataPath=trainingDataPath,batch_size=batch_size)
+    loopdeloop = loops.trainAndTest(train_dataloader,
+                                    test_dataloader,
+                                    model,
+                                    lossFn,
+                                    optimizer)
 
-print('starting training session')
-print(f"Approx training steps per epoch:{len(train_dataloader)//batch_size}")
-try:
-    for t in range(epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
-        loopdeloop.train_loop()
-        loopdeloop.test_loop()
+    print('starting training session')
+    print(f"Approx training steps per epoch:{len(train_dataloader)//batch_size}")
+    try:
+        for t in range(epochs):
+            print(f"Epoch {t+1}\n-------------------------------")
+            loopdeloop.train_loop()
+            loopdeloop.test_loop()
+            print('saving model')
+            model.saveWeights(weightsPath=clstm.modelPath)
+    except KeyboardInterrupt:
+        print('interrupted!')
         print('saving model')
-        torch.save(model.state_dict(),clstm.modelPath)
-except KeyboardInterrupt:
-    print('interrupted!')
-    print('saving model')
-    torch.save(model.state_dict(),clstm.modelPath)
-print('training session finished')
+        model.saveWeights(weightsPath=clstm.modelPath)
+    print('training session finished')
+
+
+if __name__ == "__main__":
+    main()
